80671ee6b35f0cb25f2b4d472e69d54e20eadf07
Jack Wong <jackwong@Jacks-MBP.fios-router.home>
2020-05-01 18:23:53 -0400

    Added Album cover feature

--- SpotifyClone/SpotifyClone/Views/ContentItemViewer.swift
+++ SpotifyClone/SpotifyClone/Views/ContentItemViewer.swift
@@ -9,22 +9,30 @@
 import SwiftUI
 
 struct ContentItemViewer: View {
+    
+    var topSpacerHeight: CGFloat = 400
+    
     var body: some View {
         ZStack {
             VStack {
                 Spacer()
                     .frame(height: 50)
-                Image(systemName: "music.note")
+                Image("westworld")
+                    .resizable()
+                    .frame(width: 200, height: 200)
                 Text("Title")
                 Text("Subtitle")
+                    .foregroundColor(.init(red: 0.5, green: 0.5, blue: 0.5))
                 Spacer()
             }
             ScrollView {
                 VStack(spacing: 0) {
                     HStack {
                         Spacer()
-                            .frame(height: 200)
-                            .background(Color.red.opacity(0.3))
+                            .frame(height: topSpacerHeight)
+                            .background(
+                                LinearGradient(gradient: Gradient(colors:  [.clear,.clear,.clear,.clear,.black]), startPoint: .top, endPoint: .bottom)
+                        )
                     }
                     
                     VStack {
